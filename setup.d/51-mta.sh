112edfec0272ea120498f01d7d8aa5b730259e32
Arne Johannessen <git@arne.johannessen.de>
2022-03-28 13:03:54 +0200

    Update postgrey client whitelist
    The one that comes with Debian hasn't been updated in years. (A monthly cronjob to auto-update might be a decent addition at a later point in time.)

--- setup.d/51-mta.sh
+++ setup.d/51-mta.sh
@@ -17,5 +17,17 @@ setup_patch /etc/default/postgrey || true
 setup_copy_maybe /etc/postfix/access_postgrey R
 setup_copy_maybe /root/postgreyreport.pl X
 
+if [[ -e /etc/postgrey/whitelist_clients && ! -e "$SETUPPATH/etc/postgrey/whitelist_clients" ]]
+then
+  # (skipped if the local setup provides a whitelist)
+  echo "Updating postgrey_whitelist_clients ..."
+  mkdir -p "$SETUPPATH_GLOBAL/etc/postgrey"
+  curl -fsSL https://postgrey.schweikert.ch/pub/postgrey_whitelist_clients \
+    -o "$SETUPPATH_GLOBAL/etc/postgrey/whitelist_clients" || \
+  curl -fsSL https://raw.githubusercontent.com/schweikert/postgrey/master/postgrey_whitelist_clients \
+    -o "$SETUPPATH_GLOBAL/etc/postgrey/whitelist_clients"
+fi
+setup_copy_maybe /etc/postgrey/whitelist_clients R
+
 /etc/postfix/reload
 
